79904b93408fba839871b806fa93267c108d40e6
mikobalint-szbi <miko.balint.miklos@diak.szbi-pg.hu>
2024-02-16 13:16:49 +0100

    Deleting Template components and Creating of Header

---
+++ src/lib/Header.svelte
@@ -0,0 +1,34 @@
+
+
+<div id="header">
+    <img src="IMG/logo.png" alt="Gyémánt logo" id="logo">
+    <h3>PawnHub</h3>
+</div>
+
+<style lang="scss">
+    
+    #header{
+        width: 100vw;
+        height: 65px;
+        background-color: rgb(109, 194, 106);
+        display: flex;
+        align-items: center;
+
+        h3{
+            font-size: 35px;
+            font-weight: 300;
+            margin: 0;
+        }
+        #logo{
+            height: 40px;
+            margin-left: 7px;
+            margin-right: 7px;
+        }
+    }
+</style>
+
+
+
+
+
+
